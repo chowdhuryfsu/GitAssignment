b06a70b3760cbef3d3699b15f0d71e5b3d44eef3
chowdhuryfsu <imran_cse_cuet@yahoo.com>
2016-09-18 02:03:55 -0400

    Include the modified rolegame.java file
    It has been modified for showing the output of all newly added weapons.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -9,15 +9,29 @@ package edu.fsu.cs.cen5035;
  *
  * @author Javier
  */
+
 public class RoleGame {
 
     public static void main(String[] args) {
         Weapon sword = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 20;
-        System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.\n");
         
-        //TODO: Add the remainig weapons here
-
+        //TODO: Add the remaining weapons here
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        System.out.println("Arrow has " + arrow.hit() + " of damage."); 
+        System.out.println("Arrows was able to do " + arrow.hit(armor) + " of damage due to an armor with "+ armor + " points.\n");
+        
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage."); 
+        System.out.println("Axes was able to do " + axe.hit(armor) + " of damage due to an armor with "+ armor + " points.\n");
+        
+        
+        Weapon magicstaff = WeaponFactory.getWeapon("magicstaff");
+        System.out.println("MagicStaff has " + magicstaff.hit() + " of damage.");
+        System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+ armor + " points.\n");
+  
     }
+
 }
